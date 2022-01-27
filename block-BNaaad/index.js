71654167da8dd1e4692b7024ecae15027737fb71
manojkrishnak <manojkrishna11@gmail.com>
2022-01-27 19:55:33 +0530

    add files:node:block-BNaaad

---
+++ block-BNaaad/index.js
@@ -0,0 +1,14 @@
+console.log('Welcome to Node.js');
+
+let sum = 0;
+for(let i = 1; i <= 10; i++){
+    sum += i;
+}
+console.log(sum);
+
+
+function square(a){
+    return a*a;
+}
+
+console.log(square(2))
